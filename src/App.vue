d6c88b8510a767aecb2d0942e6676ef20016ae77
lyh <1046818625@qq.com>
2022-03-21 11:23:21 +0800

    refactor: 调整页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import bg from './assets/bg.jpg'
 import './assets/china'
 import { useStore } from './stores'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import * as echarts from 'echarts'
 import { geoCoordMap } from './assets/geoMap'
 
+
 const store = useStore()
+const loading = ref(false)
+const dialogTableVisible = ref(false)
+const gridData = ref(store.item)
 
 onMounted(async () => {
+  loading.value = true
   await store.getList()
   initCharts()
   initPie()
   initLine()
+  loading.value = false
 })
 
 const initCharts = () => {
@@ -125,12 +130,13 @@ const initCharts = () => {
     ],
   })
   charts.on('click', (e: any) => {
-    store.item = e.data.children
+    gridData.value = e.data.children
+    dialogTableVisible.value = true
   })
 }
 
 const initPie = () => {
-  const charts = echarts.init(document.querySelector('.box-left-pie') as HTMLElement)
+  const charts = echarts.init(document.querySelector('.pie') as HTMLElement)
   charts.setOption({
     backgroundColor: "#223651",
     tooltip: {
@@ -166,7 +172,7 @@ const initPie = () => {
 }
 
 const initLine = () => {
-  const charts = echarts.init(document.querySelector('.box-left-line') as HTMLElement)
+  const charts = echarts.init(document.querySelector('.line') as HTMLElement)
   charts.setOption({
     backgroundColor: "#223651",
     tooltip: {
@@ -205,9 +211,9 @@ const initLine = () => {
 
 
 <template>
-  <div class="box" :style="{ background: `url(${bg})` }">
-    <div class="box-left" style="color:white">
-      <div class="box-left-card">
+   <el-row>
+    <el-col :span="24">
+      <div class="top-card" v-loading="loading">
         <section>
           <div>较上日+ {{ store.chinaAdd.localConfirmH5 }}</div>
           <div>{{ store.chinaTotal.localConfirm }}</div>
@@ -239,33 +245,31 @@ const initLine = () => {
           <div>累计死亡</div>
         </section>
       </div>
-      <div class="box-left-pie"></div>
-      <div class="box-left-line"></div>
-    </div>
-    <div class="box-center" id="china"></div>
-    <div class="box-right" style="color:white">
-      <table class="table" cellspacing="0" border="1">
-        <thead>
-          <tr>
-            <th>地区</th>
-            <th>新增确诊</th>
-            <th>累计确诊</th>
-            <th>治愈</th>
-            <th>死亡</th>
-          </tr>
-        </thead>
-        <transition-group enter-active-class="animate__animated animate__flipInY" tag="tbody">
-          <tr :key="item.name" v-for="item in store.item">
-            <td align="center">{{ item.name }}</td>
-            <td align="center">{{ item.today.confirm }}</td>
-            <td align="center">{{ item.total.confirm }}</td>
-            <td align="center">{{ item.total.heal }}</td>
-            <td align="center">{{ item.total.dead }}</td>
-          </tr>
-        </transition-group>
-      </table>
-    </div>
-  </div>
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="12">
+      <div class="pie" v-loading="loading"></div>
+    </el-col>
+    <el-col :span="12">
+      <div class="line" v-loading="loading"></div>
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="24">
+      <el-divider />
+      <div id="china" v-loading="loading"></div>
+    </el-col>
+  </el-row>
+    <el-dialog v-model="dialogTableVisible" title="详情信息">
+    <el-table :data="gridData">
+      <el-table-column property="name" label="地区" />
+      <el-table-column property="today.confirm" label="新增确诊" />
+      <el-table-column property="total.confirm" label="累计确诊" />
+      <el-table-column property="total.heal" label="治愈" />
+      <el-table-column property="total.dead" label="死亡" />
+    </el-table>
+  </el-dialog>
 </template>
 
 
@@ -279,63 +283,38 @@ const initLine = () => {
   }
   html, body, #app {
     height: 100%;
-    overflow: hidden;
   }
-  .table {
-    width: 100%;
-    background: #212028;
-    tr {
-      th {
-        padding: 5px;
-        white-space: nowrap;
-      }
-      td {
-        padding: 5px 10px;
-        width: 100px;
-        white-space: nowrap;
+  .top-card {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-template-rows: auto auto;
+    section {
+      background: @itemBg;
+      border: 1px solid @itemBorder;
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      div:nth-child(2) {
+        color: @itemColor;
+        padding: 10px 0;
+        font-size: 20px;
+        font-weight: bold;
       }
     }
   }
-  .box {
-    height: 100%;
-    display: flex;
-    overflow: hidden;
-    padding: 10px;
-    &-left {
-      width: 400px;
-      &-pie {
-        height: 320px;
-        margin-top: 20px;
-      }
-      &-line {
-        height: 320px;
-        margin-top: 20px;
-      }
-      &-card {
-        display: grid;
-        grid-template-columns: auto auto auto;
-        grid-template-rows: auto auto;
-        section {
-          background: @itemBg;
-          border: 1px solid @itemBorder;
-          padding: 10px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          div:nth-child(2) {
-            color: @itemColor;
-            padding: 10px 0;
-            font-size: 20px;
-            font-weight: bold;
-          }
-        }
-      }
-    }
-    &-center {
-      flex: 1;
-    }
-    &-right {
-      width: 450px;
-    }
+  .pie {
+    height: 250px;
+  }
+  .line {
+    height: 250px;
+  }
+  #china {
+    height: 500px;
+    background-color: @itemBg;
+  }
+  .el-divider--horizontal {
+    background-color: @itemBg;
+    margin: 0 !important;
   }
 </style>
